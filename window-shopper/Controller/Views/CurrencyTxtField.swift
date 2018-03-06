71d3977156d7138497afe1d6cb1d443168a6facc
Karl Bullock <karlbullock@Karls-MBP.home>
2018-03-06 12:34:22 +0000

    added text field created folder and wrote some code for text field

---
+++ window-shopper/Controller/Views/CurrencyTxtField.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Karl Bullock on 06/03/2018.
+//  Copyright © 2018 Karl Bullock. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2470569349)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+                attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
